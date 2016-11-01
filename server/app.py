57e5d00950bfc18321417384d9a7b6049db71be6
Ilya <scorpioni4@yandex.ru>
2016-11-01 23:20:51 +0300

    File structure reorganized.

---
+++ server/app.py
@@ -0,0 +1,39 @@
+#!bin/python3.4
+from flask import Flask
+from flask import jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/users', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+if __name__ == '__main__':
+	app.run(debug=False)
