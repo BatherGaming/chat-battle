cc6634318f0c3f55350372d790ce5281f2c26dc0
Nikolay <n-zhidkov@yandex.ru>
2016-11-06 17:11:59 +0300

    first version of chat-backend. create_chat, close_chat, get_messages, send_message are now available

--- server/db.py
+++ server/db.py
@@ -1,8 +1,9 @@
 import sqlalchemy
+import datetime
 from sqlalchemy import create_engine
 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import text
 
@@ -12,6 +13,26 @@ from sqlalchemy.sql import text
 engine = create_engine('sqlite:///chat-battle.db')
 Base = declarative_base()
 
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String, nullable=False)
+    time = Column(sqlalchemy.types.DateTime)
+    
+    chat_id = Column(Integer, ForeignKey('chats.id'))
+    sender_id = Column(Integer, ForeignKey('players.id'))
+
+   
+
+    def map_repr(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "sender_id": self.sender_id,
+            "time": str(self.time)
+        }
+
 class Player(Base):
     __tablename__ = 'players'
 
@@ -19,15 +40,31 @@ class Player(Base):
     login = Column(String)
     age = Column(Integer)
     password_hash = Column(String)
-    sex = Column(String) # 'male'/'female'
+    sex = Column(String)  # 'male'/'female'
+    chat_id = Column(Integer, ForeignKey('chats.id'))
+
+    messages = relationship("Message", backref="sender")
+
 
     def map_repr(self):
         return {"id": self.id,
                 "login": self.login,
                 "sex": self.sex,
-                "age": self.age}
+                "age": self.age,
+                "chatId": self.chat_id}
 
 
+
+class Chat(Base):
+    __tablename__ = 'chats'
+
+    id = Column(Integer, primary_key=True)
+    creation_time = Column(sqlalchemy.types.DateTime)
+    type = Column(Integer)
+    is_closed = Column(sqlalchemy.types.Boolean)
+
+    messages = relationship("Message", backref="chat")
+    players = relationship("Player", backref="chat")
 
 Base.metadata.create_all(engine)
 
