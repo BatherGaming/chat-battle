b652ed743dad8aa6d3f687eff618d0727d571232
Nikolay <n-zhidkov@yandex.ru>
2017-01-16 19:35:23 +0300

    new design of login activity

--- client/app/src/main/java/ru/spbau/shevchenko/chatbattle/frontend/LoginActivity.java
+++ client/app/src/main/java/ru/spbau/shevchenko/chatbattle/frontend/LoginActivity.java
@@ -2,12 +2,15 @@ package ru.spbau.shevchenko.chatbattle.frontend;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import ru.spbau.shevchenko.chatbattle.R;
 import ru.spbau.shevchenko.chatbattle.backend.MyApplication;
@@ -28,6 +31,13 @@ public class LoginActivity extends BasicActivity implements View.OnClickListener
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final TextView chatTextView = (TextView) findViewById(R.id.chat_text_view);
+        chatTextView.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/BetsyFlanagan.ttf"));
+
+        final TextView battleTextView = (TextView) findViewById(R.id.battle_text_view);
+        battleTextView.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/BetsyFlanagan.ttf"));
+
         showLayout(View.VISIBLE, View.GONE);
 
         if (triedAutoLogin && !autoLogin()) showLayout(View.GONE, View.VISIBLE);
