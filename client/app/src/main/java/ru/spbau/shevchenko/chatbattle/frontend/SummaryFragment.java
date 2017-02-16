c684560e054ac0bcdd8795a4960ac8cfa5e3f84f
Nikolay <n-zhidkov@yandex.ru>
2017-02-16 19:13:18 +0300

    redundant drawables and layouts were deleted. All resourses are refactored

--- client/app/src/main/java/ru/spbau/shevchenko/chatbattle/frontend/SummaryFragment.java
+++ client/app/src/main/java/ru/spbau/shevchenko/chatbattle/frontend/SummaryFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
